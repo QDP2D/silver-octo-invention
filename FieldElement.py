29017ccc026351e32cf67d8568de422e40ac9c44
qdp2d <aqdp2d@gmail.com>
2021-03-11 08:04:37 -0600

    fixed repr and added add

--- FieldElement.py
+++ FieldElement.py
@@ -1,16 +1,26 @@
 class FieldElement:
-
-    def _init_(self, num, prime):
+    def __init__(self, num, prime):
         if num >= prime or num < 0:
-            error = 'Num {} not in field range 0 to {}'.format(num, prime -1)
+            error = 'Num {} not in field range 0 to {}'.format(num, prime - 1)
             raise ValueError(error)
         self.num = num
         self.prime = prime
-    
-    def _repr_(self):
-        return 'FieldElement_{} ({})'.format(num, prime - 1)
-    
-    def _eq_ (self, other):
+
+    def __repr__(self):
+        return 'FieldElement_{} ({})'.format(self.num, self.prime - 1)
+
+    def __eq__(self, other):
         if other is None:
             return False
         return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other):
+        if other is None:
+            return True
+        return self.num != other.num or self.prime != self.prime != other.prime
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot add two numbers in different fields')
+        num = (self.num + other.num) % self.prime
+        return self.__class__(num, self.prime)
